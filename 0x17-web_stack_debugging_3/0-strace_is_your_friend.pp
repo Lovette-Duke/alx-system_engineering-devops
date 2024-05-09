b438e8da5eda79012b4dc3201b997f326a869742
Lovette Duke <qutyluv@gmail.com>
2024-05-09 02:32:46 +0100

    Task

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+#web debbuging
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,1 @@
+This folder holds the Web debbuging 3 project
